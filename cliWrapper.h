e02ed9343b7e93e40ac61f34aa54d913794d3d26
Pim Snel <pim@lingewoud.nl>
2011-07-05 17:10:37 +0200

    -improved arguments -added usage -add jpeg support

---
+++ cliWrapper.h
@@ -0,0 +1,17 @@
+//
+//  cliWrapper.h
+//  pdfimages
+//
+//  Created by Pim Snel on 05-07-11.
+//  Copyright 2011 Lingewoud B.V. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface cliWrapper : NSObject {
+@private
+    
+}
+
+@end
---
+++ cliWrapper.m
@@ -0,0 +1,29 @@
+//
+//  cliWrapper.m
+//  pdfimages
+//
+//  Created by Pim Snel on 05-07-11.
+//  Copyright 2011 Lingewoud B.V. All rights reserved.
+//
+
+#import "cliWrapper.h"
+
+
+@implementation cliWrapper
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
